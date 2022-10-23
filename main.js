997ce514b7bcfe93bfdc4ddc1e4259588275425c
Sharona Young <msrona806@gmail.com>
2022-10-22 22:28:20 -0400

    added html and js files

---
+++ main.js
@@ -0,0 +1,17 @@
+// function firstLetterName 
+const firstLetterName = (name) =>{
+  alert("The name " + name + " starts with the letter " + name[0]);
+}
+firstLetterName("Sharona");
+
+// function divisibleByTwo
+const divisibleByTwo = (number) => {
+ result = number % 2;
+ console.log(result);
+} 
+divisibleByTwo(8);
+
+
+
+
+// function largestNum
